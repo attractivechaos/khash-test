3ca9bf5faa4d34310467530f25681359901ae88b
Heng Li <lh3@me.com>
2016-10-19 23:09:11 -0400

    optionally preallocate hash tables

--- kmer-cnt.c
+++ kmer-cnt.c
@@ -66,7 +66,7 @@ unsigned char seq_nt4_table[128] = { // Table to change "ACGTN" to 01234
     4, 4, 4, 4,  3, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4
 };
 
-int count_64(const char *fn, int k, int min_occ)
+int count_64(const char *fn, int size0, int k, int min_occ)
 {
 	khash_t(64) *h;
     long l_buf, m_buf = 0, size;
@@ -76,6 +76,7 @@ int count_64(const char *fn, int k, int min_occ)
 
 	if ((fp = fn && strcmp(fn, "-")? fopen(fn, "r") : stdin) == 0) return -1;
 	h = kh_init(64);
+	if (size0 > 4) kh_resize(64, h, size0);
 	while ((l_buf = kgetline(&buf, &m_buf, fp)) != EOF) {
     	uint64_t x, mask = (1ULL<<k*2) - 1;
 		int i, l;
@@ -103,7 +104,7 @@ int count_64(const char *fn, int k, int min_occ)
 	return size;
 }
 
-int count_str(const char *fn, int k, int min_occ)
+int count_str(const char *fn, int size0, int k, int min_occ)
 {
 	khash_t(str) *h;
     long l_buf, m_buf = 0, size;
@@ -114,6 +115,7 @@ int count_str(const char *fn, int k, int min_occ)
 	if ((fp = fn && strcmp(fn, "-")? fopen(fn, "r") : stdin) == 0) return -1;
 	buf2[k] = 0;
 	h = kh_init(str);
+	if (size0 > 4) kh_resize(str, h, size0);
 	while ((l_buf = kgetline(&buf, &m_buf, fp)) != EOF) {
 		int i, l;
 		for (i = l = 0; i < l_buf; ++i) {
@@ -145,7 +147,7 @@ int count_str(const char *fn, int k, int min_occ)
 	return size;
 }
 
-int count_cache(const char *fn, int k, int min_occ)
+int count_cache(const char *fn, int size0, int k, int min_occ)
 {
 	khash_t(cache) *h;
     long l_buf, m_buf = 0, size;
@@ -158,6 +160,7 @@ int count_cache(const char *fn, int k, int min_occ)
 	buf2[k] = 0;
 	cc.s = buf2;
 	h = kh_init(cache);
+	if (size0 > 4) kh_resize(cache, h, size0);
 	while ((l_buf = kgetline(&buf, &m_buf, fp)) != EOF) {
 		int i, l;
 		for (i = l = 0; i < l_buf; ++i) {
@@ -194,32 +197,33 @@ int count_cache(const char *fn, int k, int min_occ)
 int main(int argc, char *argv[])
 {
 	clock_t t;
-	int c, k = 31, min_occ = 3, size, is_6 = 0, is_s = 0, is_c = 0;
+	int c, k = 31, min_occ = 3, size, is_6 = 0, is_s = 0, is_c = 0, size0 = 4;
 
-	while ((c = getopt(argc, argv, "k:6sc")) >= 0)
+	while ((c = getopt(argc, argv, "k:6scp:")) >= 0)
 		if (c == 'k') k = atoi(optarg);
 		else if (c == '6') is_6 = 1;
 		else if (c == 's') is_s = 1;
 		else if (c == 'c') is_c = 1;
+		else if (c == 'p') size0 = atoi(optarg);
 
 	if (argc == optind) {
-		fprintf(stderr, "Usage: kmer-cnt [-6s] [-k %d] <in.seq>\n", k);
+		fprintf(stderr, "Usage: kmer-cnt [-6s] [-k %d] [-p %d] <in.seq>\n", k, size0);
 		return 1;
 	}
 
 	if (is_6) {
 		t = clock();
-		size = count_64(argv[optind], k, min_occ);
+		size = count_64(argv[optind], size0, k, min_occ);
 		fprintf(stderr, "%d\t%.3f\n", size, (double)(clock() - t) / CLOCKS_PER_SEC);
 	}
 	if (is_s) {
 		t = clock();
-		size = count_str(argv[optind], k, min_occ);
+		size = count_str(argv[optind], size0, k, min_occ);
 		fprintf(stderr, "%d\t%.3f\n", size, (double)(clock() - t) / CLOCKS_PER_SEC);
 	}
 	if (is_c) {
 		t = clock();
-		size = count_cache(argv[optind], k, min_occ);
+		size = count_cache(argv[optind], size0, k, min_occ);
 		fprintf(stderr, "%d\t%.3f\n", size, (double)(clock() - t) / CLOCKS_PER_SEC);
 	}
 	return 0;
